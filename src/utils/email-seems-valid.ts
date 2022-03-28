3333fcd9a1ecc4e263cfd15c67adaa263e0da7bf
Dawson Botsford <dawsonbotsford@gmail.com>
2022-03-27 17:16:24 -0700

    Cleanup legacy comments from last project

--- src/utils/email-seems-valid.ts
+++ src/utils/email-seems-valid.ts
@@ -1,24 +1,14 @@
 import isEmail from 'validator/lib/isEmail';
 
 /**
- * Similar to ["parseEther" in ethers.js](https://docs.ethers.io/v4/api-utils.html#ether-strings-and-wei)
- *
- * Similar to ["toWei" in web3](https://web3js.readthedocs.io/en/v1.2.11/web3-utils.html?highlight=towei#towei)
+ * Validate if an email appears to work with providers like Amazon SES. Works offline.
  *
  * @example
  * ```javascript
- * emailSeemsValid('1000').toString()
- * // '1000000000000000000000'
- * emailSeemsValid(1000).toString()
- * // '1000000000000000000000'
- * ```
- *
- * @example
- * ```javascript
- * emailSeemsValid('1000').toNumber()
- * // 1000000000000000000000
- * emailSeemsValid(1000).toNumber()
- * // 1000000000000000000000
+ * emailSeemsValid('test@gmail.com')
+ * // true
+ * emailSeemsValid('test@gmai.com)
+ * // false
  * ```
  */
 export function emailSeemsValid(emailAddress: string): boolean {
