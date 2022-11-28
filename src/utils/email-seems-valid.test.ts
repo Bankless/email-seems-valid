9cad5a3a81017ff86af23e9d6308f9b2f334223f
dawsbot <dawsonbotsford@gmail.com>
2022-11-28 11:59:17 -0700

    Make harder test cases

--- src/utils/email-seems-valid.test.ts
+++ src/utils/email-seems-valid.test.ts
@@ -10,10 +10,10 @@ describe('form validations', () => {
   });
   it('valid emails', () => {
     expect(emailSeemsValid('test@crazy.finance')).toBe(true);
-    expect(emailSeemsValid('test@yahoo.com')).toBe(true);
-    expect(emailSeemsValid('test@yahoo.COM')).toBe(true);
-    expect(emailSeemsValid('test@gmail.com')).toBe(true);
-    expect(emailSeemsValid('test@hotmail.com')).toBe(true);
+    expect(emailSeemsValid('test.cmo@yahoo.com')).toBe(true);
+    expect(emailSeemsValid('gmial@yahoo.COM')).toBe(true);
+    expect(emailSeemsValid('gmia@gmail.com')).toBe(true);
+    expect(emailSeemsValid('hotmai@hotmail.com')).toBe(true);
 
     expect(emailSeemsValid('a@protonmail.com')).toBe(true);
     expect(emailSeemsValid('test@dawsbot.io')).toBe(true);
